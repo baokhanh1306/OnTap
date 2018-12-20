9defa1710875cc760807ac5468431e20e6f7fb61
Bao Khanh <baokhanh1306nsc@gmail.com>
2018-12-21 00:26:16 +0700

    Them getch

--- CuoiKi/main.cpp
+++ CuoiKi/main.cpp
@@ -1,9 +1,10 @@
 #include "Castle.h"
-
+#include "conio.h"
 int main(){
     Castle castle;
     castle.inputGates();
     castle.inputPrince();
     castle.hoanThanh();
+    _getch();
     return 0;
 }
